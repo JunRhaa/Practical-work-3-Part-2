efe88c8168dde276b9695396dbd4d75561cae9d5
JunRhaa <115356725+JunRhaa@users.noreply.github.com>
2025-02-21 15:56:47 +0300

    Update MainWindow.xaml.cs

--- WIndSpeedCalculator/MainWindow.xaml.cs
+++ WIndSpeedCalculator/MainWindow.xaml.cs
@@ -13,42 +13,37 @@ namespace WindSpeedCalculator
 
         private void BtnCalculate_Click(object sender, RoutedEventArgs e)
         {
-            // Проверка, что оба текстовых поля заполнены
             if (string.IsNullOrWhiteSpace(txtDistance.Text) || string.IsNullOrWhiteSpace(txtTime.Text))
             {
                 MessageBox.Show("Необходимо ввести расстояние и время.");
                 return;
             }
 
-            // Проверка, что выбран переключатель
             if (!rbMetersPerSecond.IsChecked.Value && !rbKilometersPerHour.IsChecked.Value)
             {
                 MessageBox.Show("Необходимо выбрать единицу измерения.");
                 return;
             }
 
-            // Проверка на ввод нечисловых символов
             if (!Regex.IsMatch(txtDistance.Text, @"^\d+$") || !Regex.IsMatch(txtTime.Text, @"^\d+$"))
             {
                 MessageBox.Show("Неверное значение. Повторите ввод.");
                 return;
             }
 
-            // Преобразование вводимых значений в числа
             double distance = Convert.ToDouble(txtDistance.Text);
             double time = Convert.ToDouble(txtTime.Text);
             string resultUnit = "";
             double speed = 0;
 
-            // Выполнение вычислений
             if (rbMetersPerSecond.IsChecked.Value)
             {
-                speed = distance / time; // Метры в секунду
+                speed = distance / time; 
                 resultUnit = "м/с";
             }
             else if (rbKilometersPerHour.IsChecked.Value)
             {
-                speed = (distance / time) * 3.6; // Метры в секунду в километры в час
+                speed = (distance / time) * 3.6; 
                 resultUnit = "км/ч";
             }
 
